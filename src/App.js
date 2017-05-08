f836f23cb280df0eda677a11ebf3baeadfd0d1db
Kevin Lee <kevin.lee@globalrelay.net>
2017-05-08 10:59:56 -0700

    removeboiler plate code in main view

--- src/App.js
+++ src/App.js
@@ -7,13 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <button className="btn btn-default">Search</button>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
