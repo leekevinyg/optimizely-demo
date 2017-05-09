9bab17a6f210497d0b85a9089826d259f582f100
Kevin Lee <kevin.lee@globalrelay.net>
2017-05-09 15:55:58 -0700

    add template html

--- src/App.js
+++ src/App.js
@@ -4,8 +4,38 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <button className="btn btn-default">Search</button>
+      <div className="container">
+
+	      <div className="row">
+
+	      	<div className="col-md-6">
+	      		<div className="row text-center">
+	      			<img class="logo" src="http://placehold.it/100x100" alt=""/>
+	      		</div>
+	      		<div className="row text-center">
+	      			<h2>Collect money from your group and make something happen</h2>
+	      		</div>
+	      		<div className="row text-center">
+	      			<button type="button" className="btn btn-success">Get Started</button>
+	      		</div>
+	      	</div>
+
+
+
+	      	<div className="col-md-6">
+	      		<div className="row text-center">
+	      			<img class="logo" src="http://placehold.it/100x100" alt=""/>
+	      		</div>
+	      		<div className="row text-center">
+	      			<h2>Collect money from your group and make something happen</h2>
+	      		</div>
+	      		<div className="row text-center">
+	      			<button type="button" className="btn btn-success">Get Started</button>
+	      		</div>
+	      	</div>
+
+	      </div>
+
       </div>
     );
   }
